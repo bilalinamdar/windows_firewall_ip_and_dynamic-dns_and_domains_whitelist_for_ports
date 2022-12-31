c64d0a72985309867140a833fe8a10179eea20d5
Bilal Inamdar <bilalinamdar@gmail.com>
2022-12-31 23:16:00 +0400

    Update firewall_sql_access_rule.ps1

--- firewall_sql_access_rule.ps1
+++ firewall_sql_access_rule.ps1
@@ -1,62 +1,54 @@
-# Set variables for the firewall rule and the whitelist file
 $ports = 1433
 $serviceNames = "SQL"
+## For multiple value example below
+#$ports = 1433, 3306, 8080
+#$serviceNames = "SQL", "MySQL", "HTTP"
 
-#$ports = @(1433, 3306, 8080)
-#$serviceNames = @("SQL", "MySQL", "HTTP")
-$whitelistFile = "C:\nssm\whitelist.txt"
+function Update-FirewallRule($ports, $serviceNames) {
+    for ($i = 0; $i -lt $ports.Length; $i++) {
+        # Set variables for the firewall rule
+        $serviceName = "Firewall" + $serviceNames[$i] + "Rule"
+        $serviceDisplayName = "Firewall " + $serviceNames[$i] + " Rule"
+        $ruleName = $serviceName
 
-# Check if script is running as administrator
-if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Error "This script must be run as an administrator. Please re-run the script as an administrator."
-    exit
-}
-
-# Create a function to update the firewall rule
-function Update-FirewallRule($port, $myservice) {
-    # Set variables for the firewall rule
-    $serviceName = "Firewall" + $myservice + "Rule"
-    $serviceDisplayName = "Firewall " + $myservice + " Rule"
-    $ruleName = $serviceName
-
-    # Check if the rule already exists
-    $error = ""
-    $rule = Get-NetFirewallRule -Name $ruleName -ErrorVariable error -ErrorAction SilentlyContinue
-    if ($rule) {
-        # Update the rule with the current whitelist
-        $ips = @()
-        $items = Get-Content $whitelistFile
-        foreach ($item in $items) {
-            # Check if the item is an IP address or a domain
-            if ([System.Net.IPAddress]::TryParse($item, [ref]$null)) {
-                # Add the IP address to the list
-                $ips += $item
-            } else {
-                # Resolve the domain to an IP address and add it to the list
-                $ip = [System.Net.Dns]::GetHostAddresses($item) | Select-Object -First 1
-                $ips += $ip
+        # Check if the rule already exists
+        $error = ""
+        $rule = Get-NetFirewallRule -Name $ruleName -ErrorVariable error -ErrorAction SilentlyContinue
+        if ($rule) {
+            # Update the rule with the current whitelist
+            $ips = @()
+            $items = Get-Content $whitelistFile
+            foreach ($item in $items) {
+                # Check if the item is an IP address or a domain
+                if ([System.Net.IPAddress]::TryParse($item, [ref]$null)) {
+                    # Add the IP address to the list
+                    $ips += $item
+                } else {
+                    # Resolve the domain to an IP address and add it to the list
+                    $ip = [System.Net.Dns]::GetHostAddresses($item) | Select-Object -First 1
+                    $ips += $ip
+                }
             }
-        }
-        Set-NetFirewallRule -Name $ruleName -RemoteAddress $ips
-    } else {
-        # Create the rule with the current whitelist
-        $ips = @()
-        $items = Get-Content $whitelistFile
-        foreach ($item in $items) {
-            # Check if the item is an IP address or a domain
-            if ([System.Net.IPAddress]::TryParse($item, [ref]$null)) {
-                # Add the IP address to the list
-                $ips += $item
-            } else {
-                # Resolve the domain to an IP address and add it to the list
-                $ip = [System.Net.Dns]::GetHostAddresses($item) | Select-Object -First 1
-                $ips += $ip
+            Set-NetFirewallRule -Name $ruleName -RemoteAddress $ips
+        } else {
+            # Create the rule with the current whitelist
+            $ips = @()
+            $items = Get-Content $whitelistFile
+            foreach ($item in $items) {
+                # Check if the item is an IP address or a domain
+                if ([System.Net.IPAddress]::TryParse($item, [ref]$null)) {
+                    # Add the IP address to the list
+                    $ips += $item
+                } else {
+                    # Resolve the domain to an IP address and add it to the list
+                    $ip = [System.Net.Dns]::GetHostAddresses($item) | Select-Object -First 1
+                    $ips += $ip
+                }
             }
+            New-NetFirewallRule -Name $ruleName -DisplayName $serviceDisplayName -Direction Inbound -Protocol TCP -LocalPort $ports[$i] -Action Allow -RemoteAddress $ips -Profile Public | Out-Null
         }
-        New-NetFirewallRule -Name $ruleName -DisplayName $serviceDisplayName -Direction Inbound -Protocol TCP -LocalPort $port -Action Allow -RemoteAddress $ips -Profile Public | Out-Null
     }
 }
-
 # Create a rule to block all incoming traffic on each port
 # Check if the firewall rule already exists
 for ($i = 0; $i -lt $ports.Length; $i++) {
@@ -70,10 +62,8 @@ for ($i = 0; $i -lt $ports.Length; $i++) {
     }
 }
 
-# Create or update the firewall rules for each port
+
 while ($true) {
-    for ($i = 0; $i -lt $ports.Length; $i++) {
-        Update-FirewallRule $ports[$i] $serviceNames[$i]
-    }
+    Update-FirewallRule $ports $serviceNames
     Start-Sleep -Seconds 300
 }
